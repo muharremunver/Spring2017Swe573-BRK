9cd3770f6d54fa14b15d1b3560cffeeb86df9d49
Brk <kazancibulent@gmail.com>
2017-05-08 01:19:02 +0300

    improved places end point unit tests.

--- wild-app/server/routes/place/place.spec.js
+++ wild-app/server/routes/place/place.spec.js
@@ -16,7 +16,6 @@ var chai = require('chai'),
 
 		it('it should return 500 if latitude parameter is not provided.', (done) => {
 
-
 			chai.request(app)
 			.get('/api/places')
 		    .end((err, res) => {
@@ -75,3 +74,73 @@ var chai = require('chai'),
 		});		
 
 	});
+
+	/**
+	 *	/places/detail/ endpoint
+	 *	It returns list of tweets posted and media shared around specified place.
+	 */
+	describe('/GET /places/detail/', () => {
+
+		it('it should return 500 if latitude parameter is not provided.', (done) => {
+
+			chai.request(app)
+			.get('/api/places/detail/')
+		    .end((err, res) => {
+      			
+      			res.body.should.be.a('object');
+      			res.body.should.have.property('code');
+      			res.body.should.have.property('message');
+      			res.body.should.not.have.property('data');
+      			assert.equal(res.body.code, '500');
+      			assert.equal(res.body.message, 'Place location must be provided!');
+      
+		    	done();
+		    });
+		});
+
+		it('it should return 500 if longitude parameter is not provided.', (done) => {
+
+			chai.request(app)
+			.get('/api/places/detail/')
+		    .end((err, res) => {
+      			
+      			res.body.should.be.a('object');
+      			res.body.should.have.property('code');
+      			res.body.should.have.property('message');
+      			res.body.should.not.have.property('data');
+      			assert.equal(res.body.code, '500');
+      			assert.equal(res.body.message, 'Place location must be provided!');
+      
+		    	done();
+		    });
+
+		});
+
+		it('it should return list of places around.', (done) => {
+
+			chai.request(app)
+			.get('/api/places/detail/')
+			.query({
+				lat: 33,
+			 	long: 33
+			})
+		    .end((err, res) => {
+      		
+      			res.body.should.be.a('object');
+      			res.body.should.have.property('code');
+      			res.body.should.have.property('message');
+      			res.body.should.have.property('data');
+      			res.body.data.should.be.an('object');
+      			res.body.data.should.have.property('tweets');
+      			res.body.data.should.have.property('media');
+      			res.body.data.tweets.should.be.an('array');
+      			res.body.data.media.should.be.an('array');
+      			assert.equal(res.body.code, '200');
+      			assert.equal(res.body.message, 'SUCCESS');
+      
+		    	done();
+		    });			
+
+		});		
+
+	});		
